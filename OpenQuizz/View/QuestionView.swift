13646a96c36855fbf54b571451c1d66db5d730e1
satanpetitcoeur <plantevin.contact@gmail.com>
2019-05-03 11:02:48 +0200

    work on main.stroryboard

---
+++ OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by Edouard PLANTEVIN on 03/05/2019.
+//  Copyright © 2019 Edouard PLANTEVIN. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+
+
+}
